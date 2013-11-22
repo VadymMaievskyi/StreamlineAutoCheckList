c461cb3cf3d21a108e0bfb26e128d038acc1ee66
Vadym Maievskyi <you@example.com>
2013-11-22 18:28:46 +0200

    maven project started

---
+++ src/test/java/TestLogin.java
@@ -0,0 +1,16 @@
+import org.junit.Test;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: vm
+ * Date: 11/22/13
+ * Time: 5:40 PM
+ */
+
+
+class TestLogin {
+    @Test
+    public void theUserShouldLoginWithProperLoginPass() {
+        assertTrue(true);
+    }
+}
